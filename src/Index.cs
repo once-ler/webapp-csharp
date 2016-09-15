f3d35dce171ba50d3c9a50cbe49dd15c3a2522bd
once-ler <htao@eztier.com>
2016-09-15 08:59:30 -0400

    Add entry point

---
+++ src/Index.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using webserver.host;
+using webserver.handlers;
+
+namespace webserver {
+  class index {
+
+    static void Main2(string[] args) {
+      List<IRequestHandler> _handlers = new List<IRequestHandler>();
+      // add your custom handlers
+      // _handlers.Add(new webserver.handlers.YourCustomHandler());
+      // create the async server
+      AsyncHttpListener server = new AsyncHttpListener(7777, _handlers);
+      server.Start();
+    }
+
+  }
+}
